169c46180edeb6530cd90b6710f866229a8f5c7d
OlegEngineer <oleg.oleksin@gmail.com>
2017-12-28 17:51:03 +0200

    Updated refresh button logic

--- app/assets/javascripts/application/client/controllers/dashboard_controller.js
+++ app/assets/javascripts/application/client/controllers/dashboard_controller.js
@@ -13,56 +13,6 @@
 
             if ($state.current.name == 'dashboard') {
 
-                $scope.transactions = [];
-
-                $scope.resetTransactionsFilters = function(){
-                    $scope.transactions_filters = {
-                        limit: 10
-                    };
-                    $scope.page = 1;
-                };
-
-                $scope.resetTransactionsFilters();
-
-                var timer = false;
-                $scope.$watch('transactions_filters', function () {
-                    if (timer) {
-                        $scope.page = 1;
-                        $timeout.cancel(timer)
-                    }
-                    timer = $timeout(function () {
-                        $scope.retrieveTransactions();
-                    }, 500)
-                }, true);
-
-                $scope.retrieveTransactions = function () {
-                    transactions.all({page: $scope.page, query: $scope.transactions_filters}).success(function (data) {
-                        $scope.transactions = data.data;
-                        $scope.count = data.count;
-                        var pagination = $('#transactions-pagination');
-                        pagination.empty();
-                        pagination.removeData('twbs-pagination');
-                        pagination.unbind('page');
-                        if ($scope.count > 0) {
-                            pagination.twbsPagination({
-                                totalPages: Math.ceil($scope.count / $scope.transactions_filters.limit),
-                                startPage: $scope.page,
-                                prev: '<',
-                                next: '>',
-                                first: false,
-                                last: false,
-                                visiblePages: 9,
-                                onPageClick: function (event, page) {
-                                    $scope.page = page;
-                                    $scope.retrieveTransactions();
-                                }
-                            })
-                        }
-                    }).error(function (data) {
-                    });
-                };
-
-                $scope.retrieveTransactions();
             }
 
                 $scope.buyWithEth = function () {
@@ -71,12 +21,6 @@
 
                 $scope.buyWithBtc = function () {
                   $scope.btcDialog();
-                };
-
-                $scope.refreshValues = function () {
-                  $scope.retrieveTransactions();
-                  $scope.checkValues();
-                  // $state.reload();
                 };
 
               $scope.ethDialog = function () {
--- app/assets/javascripts/application/client/controllers/home.controller.js
+++ app/assets/javascripts/application/client/controllers/home.controller.js
@@ -8,6 +8,7 @@
 
             $scope.I18n = I18n;
             $scope.$state = $state;
+            $scope.transactions = [];
 
 
             $timeout(function(){
@@ -33,6 +34,56 @@
                 }
             };
 
+            $scope.resetTransactionsFilters = function(){
+              $scope.transactions_filters = {
+                limit: 10
+              };
+              $scope.page = 1;
+            };
+
+            $scope.resetTransactionsFilters();
+
+            var timer = false;
+
+            $scope.$watch('transactions_filters', function () {
+              if (timer) {
+                $scope.page = 1;
+                $timeout.cancel(timer)
+              }
+              timer = $timeout(function () {
+                $scope.retrieveTransactions();
+              }, 500)
+            }, true);
+
+            $scope.retrieveTransactions = function () {
+              transactions.all({page: $scope.page, query: $scope.transactions_filters}).success(function (data) {
+                $scope.transactions = data.data;
+                $scope.count = data.count;
+                var pagination = $('#transactions-pagination');
+                pagination.empty();
+                pagination.removeData('twbs-pagination');
+                pagination.unbind('page');
+                if ($scope.count > 0) {
+                  pagination.twbsPagination({
+                    totalPages: Math.ceil($scope.count / $scope.transactions_filters.limit),
+                    startPage: $scope.page,
+                    prev: '<',
+                    next: '>',
+                    first: false,
+                    last: false,
+                    visiblePages: 9,
+                    onPageClick: function (event, page) {
+                      $scope.page = page;
+                      $scope.retrieveTransactions();
+                    }
+                  })
+                }
+              }).error(function (data) {
+              });
+            };
+
+            $scope.retrieveTransactions();
+
             $scope.checkValues = function () {
               transactions.getValues()
                 .success(function (data) {
@@ -42,6 +93,11 @@
 
                 });
             };
+
+              $scope.refreshValues = function () {
+                $scope.retrieveTransactions();
+                $scope.checkValues();
+              };
 
             $scope.checkSession = function(){
                 session.profile()
