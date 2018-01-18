1736b4da19d7a09e2102a71fa43aeced06bc7f30
KL <oleg.oleksin@gmail.com>
2018-01-18 12:10:35 +0200

    Change refresh button

--- app/assets/javascripts/application/controllers/home.controller.js
+++ app/assets/javascripts/application/controllers/home.controller.js
@@ -92,7 +92,7 @@
                   .success(function(data, status){
                       $scope.current_user = data;
                       $scope.checkValues();
-                      // $state.go('dashboard');
+                      $state.go('dashboard');
                   })
                   .error(function(data){
                       $scope.current_user = false;
