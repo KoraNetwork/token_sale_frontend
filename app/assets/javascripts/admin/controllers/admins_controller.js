b834bb7449a04cc5c5625919a4d93f6c7bcc0deb
SHabor <gabor_96@mail.ru>
2018-02-06 15:04:13 +0200

    Fixes.

--- app/assets/javascripts/admin/controllers/admins_controller.js
+++ app/assets/javascripts/admin/controllers/admins_controller.js
@@ -10,7 +10,10 @@
                 $scope.I18n = I18n;
                 $scope.$state = $state;
                 $scope._ = _;
-                $scope.filters = {role: "admin"};
+                $scope.admins_filters = {
+                    role: "admin",
+                    limit: 10
+                };
 
                 $scope.errors = function(data) {
                     if(data.errors){
@@ -33,9 +36,28 @@
                 }, true);
 
                 $scope.retrieveAdmins = function () {
-                    users.getUsers({page: $scope.page, query: $scope.filters}).success(function (data) {
+                    users.getUsers({page: $scope.page, query: $scope.admins_filters}).success(function (data) {
                         $scope.admins = data.data;
                         $scope.count = data.count;
+                        var pagination = $('#admins-pagination');
+                        pagination.empty();
+                        pagination.removeData('twbs-pagination');
+                        pagination.unbind('page');
+                        if ($scope.count > 0) {
+                            pagination.twbsPagination({
+                                totalPages: Math.ceil($scope.count / $scope.admins_filters.limit),
+                                startPage: $scope.page,
+                                prev: '<',
+                                next: '>',
+                                first: false,
+                                last: false,
+                                visiblePages: 9,
+                                onPageClick: function (event, page) {
+                                    $scope.page = page;
+                                    $scope.retrieveAdmins();
+                                }
+                            })
+                        }
                     }).error(function (data) {
                     });
                 };
@@ -76,6 +98,7 @@
                                 admins.postEmail(inputValue)
                                     .success(function(data){
                                         $scope.message({ message: ["Email successfully send."] });
+                                        $scope.retrieveAdmins();
                                         SweetAlert.close();
                                     })
                                     .error(function(data) {
